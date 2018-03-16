95853ca2f3cef4fbc5e5589cf1cc567f441b2318
Panchama <ks510@users.noreply.github.com>
2018-03-16 14:48:27 +0000

    GameData class finished for the InputParser
    GameData is a simple container class for storing all objects needed for making the game.

---
+++ GameData.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public class GameData
+{
+    private List<PotLuck> potLuckCards;
+    private List<OpportunityKnocks> opportunityKnocksCards;
+    private IGameSpace[] boardSpaces;
+    private int spaces;
+
+	public GameData()
+	{
+        potLuckCards = new List<PotLuck>();
+        opportunityKnocksCards = new List<OpportunityKnocks>();
+        boardSpaces = new IGameSpace[40];
+        int spaces = 0;
+	}
+    
+    
+    public void AddPotLuckCard(PotLuck card)
+    {
+        //add pot luck card to list
+        potLuckCards.Add(card);
+    }
+
+    public void AddOpportunityKnocksCard(OpportunityKnocks card)
+    {
+        //add pot luck card to list
+        opportunityKnocksCards.Add(card);
+    }
+
+    public void AddGameSpace(IGameSpace space)
+    {
+        //add gamespace to the next space in the board array
+        if (checkSpaces())
+        {
+            boardSpaces[spaces] = space;
+            IncrementSpaces();
+        }
+        
+    }
+
+    public List<PotLuck> GetPotLuckCards()
+    {
+        return potLuckCards;
+    }
+
+    public List<OpportunityKnocks> GetOpportunityKnocks()
+    {
+        return opportunityKnocksCards;
+    }
+
+    public IGameSpace[] GetBoardSpaces()
+    {
+        return boardSpaces;
+    }
+
+    private int IncrementSpaces()
+    {
+        spaces++;
+    }
+
+    private bool checkSpaces()
+    {
+        return spaces == 40;
+    }
+
+}
