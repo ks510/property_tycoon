9bdd2c6a2cb83799183f5859e29768e2a2cf3b8d
Panchama <ks510@users.noreply.github.com>
2018-03-28 23:39:15 +0100

    Card classes completed and tested by Karen
    Unit tests will be uploaded to separate folder

---
+++ AbstractCard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTycoonProject
+{
+    public abstract class AbstractCard
+    {
+        private string description;
+        private IAction action;
+
+        public AbstractCard(string description, IAction action)
+        {
+            this.description = description;
+            this.action = action;
+        }
+
+        public string GetDescription()
+        {
+            return this.description;
+        }
+
+        public IAction GetAction()
+        {
+            return this.action;
+        }
+
+        public abstract string GetCardName();
+    }
+}
---
+++ PotLuck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTycoonProject
+{
+    public class PotLuck : AbstractCard
+    {
+        private string cardName;
+
+        public PotLuck(string description, IAction action) : base(description,action)
+        {
+            this.cardName = "Pot Luck";
+        }
+
+        public override string GetCardName()
+        {
+            return this.cardName;
+        }
+    }
+}
