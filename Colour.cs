7c8e41de2d0c019628585c8aa846fd29269a39fe
Panchama <ks510@users.noreply.github.com>
2018-03-29 00:43:55 +0100

    Colour enum class tested by Karen, completed by Zach
    Unit tests uploaded to "Unit Tests" folder"

---
+++ Colour.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTycoonProject
+{
+    /// <summary>
+    /// Represents the colour group that a developable property may belong to.
+    /// The colours also store the cost of development for properties in that group.
+    /// </summary>
+    public enum Colour
+    {
+        Brown = 50,
+        Blue = 50,
+        Purple = 100,
+        Orange = 100,
+        Red = 150,
+        Yellow = 150,
+        Green = 200,
+        DeepBlue = 200,
+    }
+}
