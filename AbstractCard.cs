8df5c492725b062352768ea514a33a1cc56f8504
Panchama <ks510@users.noreply.github.com>
2018-03-29 00:08:37 +0100

    Completed and tested Card classes by Karen
    Documentation has also been added for all methods

--- AbstractCard.cs
+++ AbstractCard.cs
@@ -11,22 +11,39 @@ namespace PropertyTycoonProject
         private string description;
         private IAction action;
 
+        /// <summary>
+        /// Constructor for a Card object in Property Tycoon.
+        /// </summary>
+        /// <param name="description">Description of instructions the player must follow upon drawing the card.</param>
+        /// <param name="action">Action required</param>
         public AbstractCard(string description, IAction action)
         {
             this.description = description;
             this.action = action;
         }
 
+        /// <summary>
+        /// Get the description of instructions the player must follow.
+        /// </summary>
+        /// <returns>String description</returns>
         public string GetDescription()
         {
             return this.description;
         }
 
+        /// <summary>
+        /// Get the action object representing the actions required upon drawing the card.
+        /// </summary>
+        /// <returns>Action required</returns>
         public IAction GetAction()
         {
             return this.action;
         }
 
+        /// <summary>
+        /// Get the name of this card type.
+        /// </summary>
+        /// <returns>Name of card</returns>
         public abstract string GetCardName();
     }
 }
---
+++ PropertyTycoon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PropertyTycoonProject
+{
+    class PropertyTycoon
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
