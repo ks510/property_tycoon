1ab7981ce623eebc34c44751d87c80e62b6b3c73
Panchama <ks510@users.noreply.github.com>
2018-03-30 12:50:03 +0100

    Dice class completed and tested by Karen
    Players roll the dice by calling the static RollDice() method.

---
+++ Dice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTycoonProject
+{
+    public class Dice
+    {
+        private static Random dice = new Random();
+
+        public static int RollDice()
+        {
+            return dice.Next(1, 7);
+        }
+
+    }
+}
