7273622adcb169d1a373444814f1d8cefd16eb79
Panchama <ks510@users.noreply.github.com>
2018-03-30 12:50:33 +0100

    Unit test by Karen
    Refer to "Test Checklist" on Google drive

---
+++ UnitTests/DiceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PropertyTycoonProject;
+
+namespace PropertyTycoonTest
+{
+    [TestClass]
+    public class DiceTest
+    {
+        [TestMethod]
+        public void TestDiceRoll()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int number = Dice.RollDice();
+                Assert.IsTrue(number < 7 && number > 0, "Dice rolled out of range 1:6");
+                Console.WriteLine("The dice rolled {0}", number);
+            }
+        }
+
+        [TestMethod]
+        public void TestDiceRoll2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int number = Dice.RollDice();
+                Assert.IsTrue(number < 7 && number > 0, "Dice rolled out of range 1:6");
+                Console.WriteLine("The dice rolled {0}", number);
+            }
+        }
+    }
+}
