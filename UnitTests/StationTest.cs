f8579bfb34c520fee38bc8235a039081ef4223eb
Panchama <ks510@users.noreply.github.com>
2018-03-30 01:04:15 +0100

    Station, Utility Unit Tests by Karen (not completed)
    Need to implemented Player classes for some tests, refer to "Test Checklist" on google drive

---
+++ UnitTests/StationTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PropertyTycoonProject;
+
+namespace PropertyTycoonTest
+{
+    [TestClass]
+    public class StationTest
+    {
+        [TestMethod]
+        public void InitialiseStationWithCorrectData()
+        {
+            int price = 100;
+            string stationName = "Hove Station";
+            Station station = new Station(stationName, price);
+
+            // correct price
+            Assert.AreEqual(100, station.GetPrice());
+            // correct station name
+            Assert.AreEqual("Hove Station", station.GetPropertyName());
+
+            // correct rent table initialised
+            int[] rentTable = new int[] { 25, 50, 100, 200 };
+            var actualRentTable = station.GetRentTable();
+            for (int i = 0; i < 4; i++)
+            {
+                Assert.AreEqual(rentTable[i], actualRentTable[i]);
+            }
+
+            // check initial owner null
+            Assert.IsNull(station.GetOwner());
+            // check initialise unmortgaged
+            Assert.IsFalse(station.IsMortgaged());
+
+        }
+
+        [TestMethod]
+        public void Station_CorrectGetterMethods()
+        {
+            Station station = new Station("Falmer Station", 200);
+
+            // TODO: correct rent returned based on number of stations owned
+
+            // TODO: correct current owner player returned
+
+            // correct price returned
+            Assert.AreEqual(200, station.GetPrice());
+
+            // total value simply returns price of station
+            Assert.AreEqual(200, station.CalculateTotalValue());
+
+            // station is undevelopable so can always be sold
+            Assert.IsTrue(station.CanSellProperty());
+
+            // correct station name returned
+            Assert.AreEqual("Falmer Station", station.GetPropertyName());
+
+            // stations are undevelopable
+            Assert.IsFalse(station.IsDevelopable());
+
+        }
+
+        [TestMethod]
+        public void Station_CorrectRentReturned()
+        {
+            //TODO: if player is in jail, rent = 0
+            Station station = new Station("Brighton Station", 200);
+            //IPlayer player = new HumanPlayer();
+            //station.SetOwner(player)
+            //player.GoToJail();
+            //Assert.AreEqual(1, station.GetRent());
+
+            // if station is mortgaged, rent = 0
+            Station station2 = new Station("Falmer Station", 200);
+            station.Mortgage();
+            Assert.IsTrue(station.IsMortgaged());
+            Assert.AreEqual(0, station.GetRent());
+
+            //TODO: if player owns x stations, return correct rent:
+            // 1 station = £25
+            // 2 stations = £50
+            // 3 stations = £100
+            // 4 stations = £200
+        }
+
+        [TestMethod]
+        public void SellStation_Unmortgaged()
+        {
+            Station station = new Station("Lewes Station", 200);
+            // station can always be sold
+            Assert.IsTrue(station.CanSellProperty());
+
+            // unmortgaged property sells for original price value (£200)
+            int sellingPrice = station.SellPropertyToBank();
+            Assert.AreEqual(200, sellingPrice);
+            // station is unowned after selling
+            Assert.IsNull(station.GetOwner());
+        }
+
+        [TestMethod]
+        public void SellStation_Mortgaged()
+        {
+            Station station = new Station("Lewes Station", 200);
+            station.Mortgage();
+            // mortgage station
+            Assert.IsTrue(station.IsMortgaged());
+
+            int sellingPrice = station.SellPropertyToBank();
+            // mortgaged station sells for half of original price value
+            Assert.AreEqual(200 / 2, sellingPrice);
+            // station is unowned after selling
+            Assert.IsNull(station.GetOwner());
+            // station set to unmortgaged after sale
+            Assert.IsFalse(station.IsMortgaged());
+        }
+
+
+    }
+}
---
+++ UnitTests/UtilityTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PropertyTycoonProject;
+
+namespace PropertyTycoonTest
+{
+    [TestClass]
+    public class UtilityTest
+    {
+        [TestMethod]
+        public void InitialiseUtilityWithCorrectData()
+        {
+            Utility utility = new Utility("Edison Water", 150);
+
+            // correct name
+            Assert.AreEqual("Edison Water", utility.GetPropertyName());
+            // correct price stored
+            Assert.AreEqual(150, utility.GetPrice());
+
+            // correct multiplers initialised
+            int[] multiplers = new int[] { 4, 10 };
+            var actualMultiplers = utility.GetMultiplers();
+            for (int i = 0; i < 2; i++)
+            {
+                Assert.AreEqual(multiplers[i], actualMultiplers[i]);
+            }
+
+            // check initial owner null
+            Assert.IsNull(utility.GetOwner());
+            // check initialise unmortgaged
+            Assert.IsFalse(utility.IsMortgaged());
+        }
+
+        [TestMethod]
+        public void UtilityCorrectGetterMethods()
+        {
+            Utility utility = new Utility("Edison Waters", 150);
+
+            // TODO: correct rent multiplier returned based on number of utilities owned
+
+            // TODO: correct current owner player returned
+
+            // correct price returned
+            Assert.AreEqual(150, utility.GetPrice());
+
+            // correct total value returned when unmortgaged
+            Assert.AreEqual(150, utility.CalculateTotalValue());
+
+            // utility is undevelopable so can always be sold
+            Assert.IsTrue(utility.CanSellProperty());
+
+            // correct utility name returned
+            Assert.AreEqual("Edison Waters", utility.GetPropertyName());
+
+            // utilities are undevelopable
+            Assert.IsFalse(utility.IsDevelopable());
+
+        }
+
+        [TestMethod]
+        public void UtilityCorrectRentMultiplierReturned()
+        {
+            //TODO: if player is in jail, rent = 0
+            Utility utility = new Utility("Tesla Power Co", 150);
+            //IPlayer player = new HumanPlayer();
+            //utility.SetOwner(player)
+            //player.GoToJail();
+            //Assert.AreEqual(1, utility.GetRent());
+
+            // if utility is mortgaged, rent = 0
+            Utility utility2 = new Utility("Edison Waters", 150);
+            utility2.Mortgage();
+            Assert.IsTrue(utility2.IsMortgaged());
+            Assert.AreEqual(0, utility2.GetRent());
+
+            //TODO: if player owns x utilies, return correct rent:
+            // 1 utility = 4
+            // 2 utilities = 10
+        }
+
+        [TestMethod]
+        public void SellUtility_Unmortgaged()
+        {
+            Utility utility = new Utility("Tesla Power Co", 200);
+            // utility can always be sold
+            Assert.IsTrue(utility.CanSellProperty());
+
+            // unmortgaged utility sells for original price value (£150)
+            int sellingPrice = utility.SellPropertyToBank();
+            Assert.AreEqual(200, sellingPrice);
+            // station is unowned after selling
+            Assert.IsNull(utility.GetOwner());
+        }
+
+        [TestMethod]
+        public void SellUtility_Mortgaged()
+        {
+            Utility utility = new Utility("Edison Waters", 150);
+            utility.Mortgage();
+            // mortgage utility
+            Assert.IsTrue(utility.IsMortgaged());
+
+            int sellingPrice = utility.SellPropertyToBank();
+            // mortgaged utility sells for half of original price value
+            Assert.AreEqual(150 / 2, sellingPrice);
+            // utility is unowned after selling
+            Assert.IsNull(utility.GetOwner());
+            // utility is unmortgaged after sale
+            Assert.IsFalse(utility.IsMortgaged());
+        }
+    }
+}
