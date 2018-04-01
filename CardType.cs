7e4744f36ca6517de7bbfc59b7062b2fbdd04e09
Panchama <ks510@users.noreply.github.com>
2018-04-01 02:35:53 +0100

    Helper enum for card actions

---
+++ CardType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTycoonProject
+{
+    public enum CardType
+    {
+        PotLuck, OpportunityKnocks
+    }
+}
