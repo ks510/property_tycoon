4163e2af8ce5554d76a75c38048d16512b6fed66
Panchama <ks510@users.noreply.github.com>
2018-04-07 16:37:36 +0100

    Finished and tested by Karen

--- GameData.cs
+++ GameData.cs
@@ -1,67 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-public class GameData
+namespace PropertyTycoonProject
 {
-    private List<PotLuck> potLuckCards;
-    private List<OpportunityKnocks> opportunityKnocksCards;
-    private IBoardSpace[] boardSpaces;
-    private int spaces;
-
-	public GameData()
-	{
-        potLuckCards = new List<PotLuck>();
-        opportunityKnocksCards = new List<OpportunityKnocks>();
-        boardSpaces = new IBoardSpace[40];
-        int spaces = 0;
-	}
-    
-    
-    public void AddPotLuckCard(PotLuck card)
+    public class GameData
     {
-        //add pot luck card to list
-        potLuckCards.Add(card);
-    }
+        private List<PotLuck> potLuckCards;
+        private List<OpportunityKnocks> opportunityKnocksCards;
+        private IBoardSpace[] boardSpaces;
+        private int spaces;
 
-    public void AddOpportunityKnocksCard(OpportunityKnocks card)
-    {
-        //add pot luck card to list
-        opportunityKnocksCards.Add(card);
-    }
-
-    public void AddGameSpace(IBoardSpace space)
-    {
-        //add board space to the next space in the board array
-        if (checkSpaces())
+        /// <summary>
+        /// Constructor a container object for the game data
+        /// </summary>
+        public GameData()
         {
-            boardSpaces[spaces] = space;
-            IncrementSpaces();
+            potLuckCards = new List<PotLuck>();
+            opportunityKnocksCards = new List<OpportunityKnocks>();
+            boardSpaces = new IBoardSpace[40];
+            spaces = 0;
         }
-        
-    }
 
-    public List<PotLuck> GetPotLuckCards()
-    {
-        return potLuckCards;
-    }
+        /// <summary>
+        /// Add the given card to the correct card pile for the game (Pot Luck or Opportunity Knocks)
+        /// </summary>
+        /// <param name="card">Card to add to game</param>
+        public void AddCard(AbstractCard card)
+        {
+            // check which type of card this is and add to correct list
+            if (card.GetType() == typeof(PotLuck))
+            {
+                potLuckCards.Add((PotLuck)card);
+            }
+            else
+            {
+                opportunityKnocksCards.Add((OpportunityKnocks)card);
+            }
+        }
 
-    public List<OpportunityKnocks> GetOpportunityKnocks()
-    {
-        return opportunityKnocksCards;
-    }
+        /// <summary>
+        /// Add the given board space to the game data
+        /// </summary>
+        /// <param name="space">Board space</param>
+        public void AddBoardSpace(IBoardSpace space)
+        {
+            //add board space to the next space in the board array
+            if (CheckFullSpaces())
+            {
+                boardSpaces[spaces] = space;
+                IncrementSpaces();
+            }
 
-    public IBoardSpace[] GetBoardSpaces()
-    {
-        return boardSpaces;
-    }
+        }
 
-    private int IncrementSpaces()
-    {
-        spaces++;
-    }
+        /// <summary>
+        /// Return the list of Pot Luck cards that will be added to the game
+        /// </summary>
+        /// <returns>Pot Luck card list</returns>
+        public List<PotLuck> GetPotLuckCards()
+        {
+            return potLuckCards;
+        }
 
-    private bool checkSpaces()
-    {
-        return spaces == 40;
-    }
+        /// <summary>
+        /// Return the list of Opportunity Knocks cards that will be added to the game
+        /// </summary>
+        /// <returns>Opportunity Knocks card list</returns>
+        public List<OpportunityKnocks> GetOpportunityKnocksCards()
+        {
+            return opportunityKnocksCards;
+        }
 
+        /// <summary>
+        /// Return the array of all board spaces that will be added to the game
+        /// </summary>
+        /// <returns></returns>
+        public IBoardSpace[] GetBoardSpaces()
+        {
+            return boardSpaces;
+        }
+
+        // increment the total number of board spaces that will be in the game
+        private void IncrementSpaces()
+        {
+            spaces++;
+        }
+
+        /// <summary>
+        /// Check if the board is full (40 spaces).
+        /// </summary>
+        /// <returns>True if board is full, false otherwise.</returns>
+        public bool CheckFullSpaces()
+        {
+            return spaces == 40;
+        }
+
+    }
 }
