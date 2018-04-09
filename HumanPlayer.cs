a63a7a671bda15ffb5c902ab651b53006ee233c1
Panchama <ks510@users.noreply.github.com>
2018-04-09 01:16:29 +0100

    Almost complete, still needs more testing (Karen)

---
+++ HumanPlayer.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTycoonProject
+{
+    public class HumanPlayer : IPlayer
+    {
+        private static bool debug = true; //debugging purposes
+
+        private int cash;
+        private int currentSpace;
+        private bool inJail;
+        private int turnsInJail;
+        private int jailFreeCards;
+        private List<IProperty> propertiesOwned;
+        private int stations;
+        private int utilities;
+        private int doublesRolled;
+        private bool rolledDouble;
+        private bool bankrupt;
+        private bool passedFirstGo;
+        private string playerName;
+        private int playerID;
+        private Token token;
+
+        /// <summary>
+        /// Constructor for a human player in the Property Tycoon game.
+        /// </summary>
+        /// <param name="name">Player's name</param>
+        /// <param name="playerID">Player ID</param>
+        /// <param name="token">Token that represents this player</param>
+        public HumanPlayer(string playerName, int playerID, Token token)
+        {
+            this.cash = 1500;
+            this.currentSpace = 0;
+            this.inJail = false;
+            this.turnsInJail = 0;
+            this.jailFreeCards = 0;
+            this.propertiesOwned = new List<IProperty>();
+            this.stations = 0;
+            this.utilities = 0;
+            this.doublesRolled = 0;
+            this.rolledDouble = false;
+            this.bankrupt = false;
+            this.passedFirstGo = false;
+            this.playerName = playerName;
+            this.playerID = playerID;
+            this.token = token;
+        }
+
+        public int RollDice()
+        {
+            int dice1 = Dice.RollDice();
+            int dice2 = Dice.RollDice();
+
+            if (debug)
+            {
+                Console.WriteLine(dice1);
+                Console.WriteLine(dice2);
+            }
+
+            // check if player rolled double and update variables accordingly
+            if (dice1 == dice2)
+            {
+                rolledDouble = true;
+                doublesRolled++;
+
+                if (debug)
+                {
+                    Console.WriteLine("Rolled Double, rolledDouble: {0}, doublesRolled: {1}", rolledDouble, doublesRolled);
+                }
+            }
+            // return the sum of the player's dice rolls
+            return dice1 + dice2;
+        }
+
+        public bool RolledDouble()
+        {
+            return this.rolledDouble;
+        }
+
+        public int GetDoublesRolled()
+        {
+            return this.doublesRolled;
+        }
+
+        public void ResetDoublesRolled()
+        {
+            this.doublesRolled = 0;
+        }
+
+        public void MoveToken(int spaces)
+        {
+            currentSpace = (currentSpace + spaces) % 40;
+        }
+
+        public int GetCurrentSpace()
+        {
+            return this.currentSpace;
+        }
+
+        public void MoveToSpace(int spaceID)
+        {
+            this.currentSpace = spaceID;
+        }
+
+        public void GoToJail(int jailSpaceID)
+        {
+            this.inJail = true;
+            this.currentSpace = jailSpaceID;
+        }
+
+        public bool InJail()
+        {
+            return this.inJail;
+        }
+
+        public void ReleaseFromJail()
+        {
+            this.inJail = false;
+            this.turnsInJail = 0;
+        }
+
+        public void IncrementTurnsInJail()
+        {
+            this.turnsInJail++;
+        }
+
+        public int GetTurnsInJail()
+        {
+            return this.turnsInJail;
+        }
+
+        public void ResetTurnsInJail()
+        {
+            this.turnsInJail = 0;
+        }
+
+        public void UseJailFreeCard()
+        {
+            // if player has at least one card, use a card by decrementing
+            if(jailFreeCards > 0)
+            {
+                jailFreeCards--;
+            }
+        }
+
+        public int GetJailFreeCards()
+        {
+            return this.jailFreeCards;
+        }
+
+        public void AddJailFreeCard()
+        {
+            this.jailFreeCards++;
+        }
+
+        public bool HasJailFreeCard()
+        {
+            return jailFreeCards > 0;
+        }
+
+        public void SetBankrupt()
+        {
+            this.bankrupt = true;
+        }
+
+        public bool IsBankrupt()
+        {
+            return this.bankrupt;
+        }
+
+        public List<IProperty> GetPropertiesOwned()
+        {
+            return this.propertiesOwned;
+        }
+
+
+        public int PeekCash()
+        {
+            return this.cash;
+        }
+
+        public void ReceiveCash(int amount)
+        {
+            cash = cash + amount;
+        }
+
+        public void DeductCash(int amount)
+        {
+            cash = cash - amount;
+        }
+
+        public int EmptyCash()
+        {
+            int totalCash = this.cash;
+            this.cash = 0;
+            return totalCash;
+        }
+
+        public void BuyProperty(IProperty property)
+        {
+            DeductCash(property.GetPrice());
+            property.SetOwner(this);
+            propertiesOwned.Add(property);
+
+            // if station or utility, update variables
+            if (property.GetType() == typeof(Station))
+            {
+                stations++;
+            }
+            else if (property.GetType() == typeof(Utility))
+            {
+                utilities++;
+            }
+        }
+
+        public void SellProperty(IProperty property)
+        {
+            ReceiveCash(property.CalculateTotalValue()); // player receives full or half value if mortgaged
+            property.SetOwner(null); // property is unowned
+            property.Unmortgage();   // ensures property is always unmortgaged after sale
+            propertiesOwned.Remove(property); // property removed from player's possession
+
+            // if station or utility, update variables
+            if (property.GetType() == typeof(Station))
+            {
+                stations--;
+            }
+            else if (property.GetType() == typeof(Utility))
+            {
+                utilities--;
+            }
+        }
+
+        public void Mortgage(IProperty property)
+        {
+            if (propertiesOwned.Contains(property) && !property.IsMortgaged())
+            {
+                property.Mortgage();
+                ReceiveCash(property.GetPrice() / 2);   // half of property value received in cash excluding improvements
+            }
+        }
+
+        public void Unmortgage(IProperty property)
+        {
+            if(propertiesOwned.Contains(property) && property.IsMortgaged())
+            {
+                DeductCash(property.GetPrice() / 2);    // pay half of property value to unmortgage
+                property.Unmortgage();  // set property to unmortgaged
+            }
+        }
+
+        public void TradeProperty(IProperty owned, IProperty newProperty)
+        {
+            propertiesOwned.Remove(owned);
+            // update if old property was a station or utility
+            if (owned.GetType() == typeof(Station))
+            {
+                stations--;
+            }
+            else if (owned.GetType() == typeof(Utility))
+            {
+                utilities--;
+            }
+
+            propertiesOwned.Add(newProperty);
+            newProperty.SetOwner(this);
+
+            // update if new property is a station or utility
+            if (newProperty.GetType() == typeof(Station))
+            {
+                stations++;
+            }
+            else if (newProperty.GetType() == typeof(Utility))
+            {
+                utilities++;
+            }
+        }
+
+        public int GetNumberOfStations()
+        {
+            return this.stations;
+        }
+
+        public int GetNumberOfUtilities()
+        {
+            return this.utilities;
+        }
+
+
+        public string GetPlayerName()
+        {
+            return this.playerName;
+        }
+
+        public Token GetToken()
+        {
+            return this.token;
+        }
+
+        public int Bid()
+        {
+            // need to consider how to connect bidding from player to auction
+            return -1;
+        }
+    }
+}
--- IPlayer.cs
+++ IPlayer.cs
@@ -1,28 +1,190 @@
-﻿namespace PropertyTycoonProject.Assets
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTycoonProject
 {
     public interface IPlayer
     {
-        void RollDice();
-        void MoveToken(int numberOfSpaces);
-        void BuyProperty(IProperty player);
-        void SellProperty(IProperty player);
-        void BuyHouse(IProperty player);
-        void Bid(int amount);
-        void PayRent(IProperty);
-        void PayFine(int amount);
-        void PayJailFine(int amount);
-        void ReceiveCash(int amount);
-        void DrawCard(bool potLuck);
-        bool InJail();
-        bool PassedFirstGo();
+        /// <summary>
+        /// Player rolls the dice for their turn and returns the sum of the dice. 
+        /// If the player rolls a double, increment the total doubles rolled for this turn.
+        /// The actual number rolled for each dice will not be stored.
+        /// </summary>
+        /// <returns>Sum of the two dice rolled.</returns>
+        int RollDice();
 
-        Token GetToken();
-        bool FinishedTurn();
-        int DoublesRolled();
-        int GetTotalValue();
-        int GetCurrentSpace();
-        void SetBankrupt(bool bankrupt);
+        /// <summary>
+        /// Check if the player rolled a double in the current turn.
+        /// </summary>
+        /// <returns></returns>
+        bool RolledDouble();
+
+        /// <summary>
+        /// Move this player's token the given number of spaces on the board.
+        /// Player moves forward if the number is positive and backwards if negative.
+        /// </summary>
+        /// <param name="spaces">Number of spaces to move</param>
+        void MoveToken(int spaces);
+
+        /// <summary>
+        /// Move this player's token to the given space on the board. 
+        /// </summary>
+        /// <param name="spaceID"></param>
+        void MoveToSpace(int spaceID);
+
+        /// <summary>
+        /// Reset the number of doubles rolled by the player in the current turn to 0.
+        /// </summary>
+        void ResetDoublesRolled();
+
+        /// <summary>
+        /// Return the number of doubles rolled by the player in the current turn.
+        /// </summary>
+        int GetDoublesRolled();
+
+        /// <summary>
+        /// Sends the player to jail.
+        /// <paramref name="jailSpaceID"/>Board space ID of jail space</param>
+        /// </summary>
+        void GoToJail(int jailSpaceID);
+
+        /// <summary>
+        /// Release the player from jail. Player moves to the "Just Visiting".
+        /// </summary>
+        void ReleaseFromJail();
+
+        /// <summary>
+        /// Check if the player is currently in jail.
+        /// </summary>
+        /// <returns>True if player is in jail, false otherwise</returns>
+        bool InJail();
+
+        /// <summary>
+        /// Increment the number of turns the player has spent in jail.
+        /// </summary>
+        void IncrementTurnsInJail();
+
+        /// <summary>
+        /// Return the number of turns the player has spent in jail.
+        /// </summary>
+        /// <returns>Number of turns in jail.</returns>
+        int GetTurnsInJail();
+
+        /// <summary>
+        /// Use a jail free card if this player has at least one jail free card.
+        /// </summary>
+        void UseJailFreeCard();
+
+        /// <summary>
+        /// Player receives a Jail Free card.
+        /// </summary>
+        void AddJailFreeCard();
+
+        /// <summary>
+        /// Check if this player has at least one jail free card.
+        /// </summary>
+        /// <returns>True if player has at least one jail free card, false otherwise.</returns>
+        bool HasJailFreeCard();
+
+        /// <summary>
+        /// Player becomes bankrupt and will not take anymore turns for the rest of the game.
+        /// </summary>
+        void SetBankrupt();
+
+        /// <summary>
+        /// Check if the player is bankrupt or not.
+        /// </summary>
+        /// <returns>True if player is bankrupt, false otherwise.</returns>
         bool IsBankrupt();
 
+        /// <summary>
+        /// This player buys the given property. The price of the property is deducted from the player's cash.
+        /// </summary>
+        /// <param name="property">Property to buy</param>
+        void BuyProperty(IProperty property);
+
+        /// <summary>
+        /// This player sells the given property. The player receives the price of the property in cash or 
+        /// half of the price if the property was mortaged.
+        /// </summary>
+        /// <param name="property">Property to sell</param>
+        void SellProperty(IProperty property);
+
+        /// <summary>
+        /// Return all of the properties owned by this player.
+        /// </summary>
+        /// <returns>All properties owned by player.</returns>
+        List<IProperty> GetPropertiesOwned();
+
+        /// <summary>
+        /// Trade one of the player's properties for a property from another player. 
+        /// </summary>
+        /// <param name="owned">The property currently owned by the player to be traded.</param>
+        /// <param name="newProperty">The property the player is trading for from another player.</param>
+        void TradeProperty(IProperty owned, IProperty newProperty);
+        
+        /// <summary>
+        /// Return the total number of stations owned by this player.
+        /// </summary>
+        /// <returns></returns>
+        int GetNumberOfStations();
+
+        /// <summary>
+        /// Return the total number of utilities owned by this player.
+        /// </summary>
+        /// <returns></returns>
+        int GetNumberOfUtilities();
+
+        /// <summary>
+        /// Return the total cash held by this player.
+        /// </summary>
+        /// <returns>Total cash held by player</returns>
+        int PeekCash();
+
+        /// <summary>
+        /// Player receives the given amount of cash.
+        /// </summary>
+        /// <param name="amount">Amount of cash to receive</param>
+        void ReceiveCash(int amount);
+
+        /// <summary>
+        /// Player's cash is decreased by the given amount.
+        /// </summary>
+        /// <param name="amount">Amount of cash to deduct</param>
+        void DeductCash(int amount);
+
+        /// <summary>
+        /// Return the player's total cash held and reset it to 0.
+        /// </summary>
+        /// <returns></returns>
+        int EmptyCash();
+
+        /// <summary>
+        /// Return the name of this player.
+        /// </summary>
+        /// <returns>Name of player</returns>
+        string GetPlayerName();
+
+        /// <summary>
+        /// Return the token representing this player.
+        /// </summary>
+        /// <returns>Player's token</returns>
+        Token GetToken();
+
+        /// <summary>
+        /// Mortgage the given property if the player owns it and receives half of the property's
+        /// original value in cash.
+        /// </summary>
+        /// <param name="property">Property to mortgage</param>
+        void Mortgage(IProperty property);
+
+        /// <summary>
+        /// Unmortgage the given property if it is mortgaged.
+        /// </summary>
+        /// <param name="property">Property to unmortgage</param>
+        void Unmortgage(IProperty property);
     }
 }
