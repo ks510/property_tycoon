3ca68e6aa3cdc8f3d0c2fe0f8bdeda374c0a231b
Panchama <ks510@users.noreply.github.com>
2018-04-15 15:21:01 +0100

    More unit tests

---
+++ UnitTests/BoardSpaceTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PropertyTycoonProject;
+
+namespace PropertyTycoonTest
+{
+    [TestClass]
+    public class BoardSpaceTest
+    {
+        [TestMethod]
+        public void CreatePropertySpace()
+        {
+            Utility waters = new Utility("Edison Waters", 5);
+            PropertySpace boardSpace = new PropertySpace(waters);
+
+            // correct property stored
+            Assert.AreEqual(waters, boardSpace.GetProperty());
+            // implements IBoardSpace interface
+            Assert.IsTrue(boardSpace is IBoardSpace);
+        }
+
+        [TestMethod]
+        public void CreateGoSpace()
+        {
+            // go space does nothing, simply an empty object
+            GoSpace go = new GoSpace();
+
+            // implements IBoardSpace interface
+            Assert.IsTrue(go is IBoardSpace);
+        }
+
+        [TestMethod]
+        public void CreateJailSpace()
+        {
+            // jail space does nothing, simply an empty object
+            JailSpace jailSpace = new JailSpace();
+
+            // implements IBoardSpace interface
+            Assert.IsTrue(jailSpace is IBoardSpace);
+
+        }
+
+        [TestMethod]
+        public void CreateFreeParkingSpace()
+        {
+            FreeParkingSpace freeParking = new FreeParkingSpace();
+
+            // implements IBoardSpace interface
+            Assert.IsTrue(freeParking is IBoardSpace);
+        }
+
+        [TestMethod]
+        public void CreateInstructionSpace()
+        {
+            PayAction pay200 = new PayAction(200, Recipient.Bank);
+            InstructionSpace payTax = new InstructionSpace(pay200);
+
+            // correct instruction stored
+            Assert.AreEqual(pay200, payTax.GetInstruction());
+            // implements IBoardSpace interface
+            Assert.IsTrue(payTax is IBoardSpace);
+
+        }
+    }
+}
---
+++ UnitTests/BoardTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PropertyTycoonProject;
+
+namespace PropertyTycoonTest
+{
+    [TestClass]
+    public class BoardTest
+    {
+        [TestMethod]
+        public void CreateBoard()
+        {
+            IBoardSpace[] boardSpaces = new IBoardSpace[] { new GoSpace(), new FreeParkingSpace() };
+            Board board = new Board(boardSpaces);
+
+            // correct spaces stored and retrieved
+            IBoardSpace space = board.GetSpace(0);
+            Assert.IsTrue(space is GoSpace);
+            space = board.GetSpace(1);
+            Assert.IsTrue(space is FreeParkingSpace);
+            // free parking starts with £0
+            Assert.AreEqual(0, board.PeekFreeParking());
+
+        }
+
+        [TestMethod]
+        public void Board_FreeParking()
+        {
+            IBoardSpace[] boardSpaces = new IBoardSpace[40];
+            Board board = new Board(boardSpaces);
+            // free parking starts at £0
+            Assert.AreEqual(0, board.PeekFreeParking());
+            // add £50 to free parking
+            board.AddToFreeParking(50);
+            Assert.AreEqual(50, board.PeekFreeParking());
+            // add another £200 to free parking
+            board.AddToFreeParking(200);
+            Assert.AreEqual(250, board.PeekFreeParking());
+
+            // receive and empty free parking funds
+            int funds = board.ReceiveFreeParking();
+            Assert.AreEqual(250, funds);
+            Assert.AreEqual(0, board.PeekFreeParking());
+        }
+    }
+
+}
