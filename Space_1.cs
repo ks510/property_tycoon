b6b4b0bc9e09fda4814c8a5215330f2c9ad3076f
Panchama <ks510@users.noreply.github.com>
2018-04-27 02:15:23 +0100

    Dynamically generates text on board spaces
    Duplicate and rename the script for each board space, changing the spaceID as well

---
+++ Space_1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PropertyTycoonLibrary;
+using UnityEngine.UI;
+using TMPro;
+
+public class Space_1 : MonoBehaviour {
+
+
+    public IBoardSpace space;
+    private int spaceID = 1;
+
+    // Use this for initialization
+    void Start () {
+        GameObject controller = GameObject.Find("GameController");
+        ControllerScript controllerScript = controller.GetComponent<ControllerScript>();
+        PropertyTycoon game = controllerScript.game;
+        space = game.GetBoardSpace(spaceID);
+        Debug.Log(space.GetType());
+
+        // dynamic text based on type of board space
+        // assumes you're using Text Mesh Pro UGUI component
+        if (space.GetType() == typeof(GoSpace))
+        {
+            GetComponent<TextMeshProUGUI>().text = spaceID + "\n\nGo!";
+            Debug.Log("Found a go space");
+        }
+        else if (space.GetType() == typeof(JailSpace))
+        {
+            GetComponent<TextMeshProUGUI>().text = "Just Visiting / Jail Space";
+        }
+        else if (space.GetType() == typeof(FreeParkingSpace))
+        {
+            GetComponent<TextMeshProUGUI>().text = "Free Parking";
+        }
+        else if (space.GetType() == typeof(PropertySpace))
+        {
+            // cast and get property object and display name
+            PropertySpace propertySpace = (PropertySpace)space;
+            IProperty property = propertySpace.GetProperty();
+            GetComponent<TextMeshProUGUI>().text = spaceID + "\n\n\n" + property.GetPropertyName();
+        }
+        else if (space.GetType() == typeof(InstructionSpace))
+        {
+            // cast and get instruction object and display description
+            InstructionSpace instructionSpace = (InstructionSpace)space;
+            string description = instructionSpace.GetDescription();
+            GetComponent<TextMeshProUGUI>().text = spaceID + "\n\n" + description;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
